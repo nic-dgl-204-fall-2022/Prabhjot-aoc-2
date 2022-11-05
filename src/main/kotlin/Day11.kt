6de8e039ab1fc09f2f793c7a33be25865935e393
Prabhjot <61209227+08prabhjot@users.noreply.github.com>
2022-11-05 18:17:44 +0530

    added 'not null'

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,28 +1,28 @@
 import java.util.regex.Pattern
 
-// First solution
 
-class Day11 : Data<String> {
 
-    private fun threeLetters(word: String): Boolean {
+class Day11 : Data<String?> {
+
+    private fun threeLetters(word: String?): Boolean {
         val letters = ".*(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz).*"
         val pattern = Pattern.matches(letters, word)
         return pattern
     }
 
-    private fun mistakenLetters(word: String): Boolean {
+    private fun mistakenLetters(word: String?): Boolean {
         val letters = ".*[iol]"
         val pattern = Pattern.matches(letters, word)
         return !pattern
     }
 
-    private fun overlappingPairs(word: String): Boolean {
+    private fun overlappingPairs(word: String?): Boolean {
         val q = ".*(.)\\1.*(.)\\2.*"
         val pattern = Pattern.matches(q, word)
         return pattern
     }
 
-    private fun isCorrect(word: String): Boolean {
+    private fun isCorrect(word: String?): Boolean {
 
         val m = threeLetters(word)
         val n = mistakenLetters(word)
@@ -33,24 +33,24 @@ class Day11 : Data<String> {
         return bool
     }
 
-    private fun addition(word: String): String {
+    private fun addition(word: String?): String {
 
-        val letters = java.lang.Long.toString(word.toLong(36) + 1, 36)
+        val letters = java.lang.Long.toString(word!!.toLong(36) + 1, 36)
             .replace('0', 'a')
 
         return letters
     }
 
-    override fun part1(dataInput: List<String>): String {
-        var passCode = addition(dataInput[0])
+    override fun part1(dataInput: List<String?>?): String {
+        var passCode = addition(dataInput!![0])
         while (!isCorrect(passCode)) {
             passCode = addition(passCode)
         }
         return passCode
     }
 
-    override fun part2(dataInput: List<String>): String {
-        var passCode = addition(dataInput[0])
+    override fun part2(dataInput: List<String?>?): String {
+        var passCode = addition(dataInput!![0])
         while (!isCorrect(passCode)) {
             passCode = addition(passCode)
         }
@@ -63,6 +63,9 @@ class Day11 : Data<String> {
 }
 
 interface Data<T> {
-    fun part1(dataInput: List<String>): T
-    fun part2(dataInput: List<String>): T
+    fun part1(dataInput: List<String?>?): T
+    fun part2(dataInput: List<String?>?): T
+}
+fun main(){
+    Day1()
 }
